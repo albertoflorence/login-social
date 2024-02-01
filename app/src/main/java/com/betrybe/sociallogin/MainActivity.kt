ca9fa4b800a9e55ed5bab23bc2718e2b5396e32b
albertoflorence <alberto.sabatier@gmail.com>
2024-02-01 19:08:47 -0300

    requisito 7

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.util.Patterns
+import android.view.KeyEvent
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher {
+
+
+    private val emailTextField: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
+    private val passwordTextField: TextInputLayout by lazy { findViewById(R.id.password_text_input_layout) }
+    private val loginButton: Button by lazy { findViewById(R.id.login_button) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailTextField.editText?.addTextChangedListener(this)
+        passwordTextField.editText?.addTextChangedListener(this)
+
+    }
+
+    private fun validateEmail(): Boolean {
+        val email = emailTextField.editText?.text.toString().trim()
+        return email.isNotEmpty()
+    }
+
+    private fun validatePassword(): Boolean {
+        val password = passwordTextField.editText?.text.toString().trim()
+        return password.isNotEmpty()
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+    override fun afterTextChanged(p0: Editable?) {
+        loginButton.isEnabled = validateEmail() && validatePassword()
     }
 }
